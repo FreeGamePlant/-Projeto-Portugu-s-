cf872708cc08d16cf661bc6a1f1c87633a653126
FGP <elaifigueroa@gmail.com>
2025-06-17 21:38:07 -0300

    Add files via upload

---
+++ src/script/js/main.js
@@ -0,0 +1,3 @@
+        console.log("2025 FGP - Todos os direitos reservados.");
+        console.log("Site desenvolvido pela FGP. programador (Elai Leonel FIgueroa) , Autor do site (Angela Patricia).");
+        console.log("FGP - Free Game Plant 'Presents'")
